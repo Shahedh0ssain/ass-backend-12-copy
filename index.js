c3299bbd81ecdbe376d47efc22fcb8facea18f74
shahed hossain <shahed0hossain@gmail.com>
2023-02-21 23:28:54 +0600

    test

--- index.js
+++ index.js
@@ -93,6 +93,7 @@ async function run() {
             const order = req.body;
             const price = order.Price;
             const amount = price * 100;
+            // console.log(amount)
             // Create a PaymentIntent with the order amount and currency
             const paymentIntent = await stripe.paymentIntents.create({
                 currency: "usd",
