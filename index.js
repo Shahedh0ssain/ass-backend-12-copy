c38474819d2cb59ea4cbc8b6c1b001c0dbaa9e9e
Shahed Hossain <96790670+Shahedh0ssain@users.noreply.github.com>
2023-02-21 12:23:20 +0600

    order api add

--- index.js
+++ index.js
@@ -76,7 +76,15 @@ async function run() {
             const isAdmin = result.role === 'admin';
             res.send({ admin: isAdmin });
         })
-
+        
+         //get order dfdfdfdf
+        app.get('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: ObjectId(id) };
+            const order = await bookingCollection.findOne(query);
+            res.send(order);
+        })
 
 
         //service  post api :
